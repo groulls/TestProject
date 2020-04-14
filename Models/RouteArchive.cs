9e49820c268b57c77df080b505d51bd67ea8e99f
Гоштаутас Максим <goshtaut@ya.ru>
2020-04-14 19:22:23 +0300

    #DAY1 Создание модели RouteArchive.cs
    Создание модели для  построяния таблицы: "Архив маршрутов"

---
+++ Models/RouteArchive.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TEST.Models
+{
+    public class RouteArchive
+    {
+        int RouteArchive_PK_ID { get; set; }
+        int UserID_FK { get; set; }
+        string RouteName { get; set; }
+        DateTime dateTime { get; set; }
+        string RouteComment { get; set; }
+        int RouteLenght { get; set; }
+        
+    }
+}
