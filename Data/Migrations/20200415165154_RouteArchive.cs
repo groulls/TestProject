c8ed9d917569e67528cfeed882412e9bf16d57f3
Гоштаутас Максим <goshtaut@ya.ru>
2020-04-15 19:54:03 +0300

    #DAY2 Add-Migration RouteArchive + Update DB
    В результате миграции Создалась таблица в БД

---
+++ Data/Migrations/20200415165154_RouteArchive.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TEST.Data.Migrations
+{
+    public partial class RouteArchive : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RouteArchive",
+                columns: table => new
+                {
+                    RouteArchiveId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RouteName = table.Column<string>(nullable: true),
+                    dateTime = table.Column<DateTime>(nullable: false),
+                    RouteComment = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RouteArchive", x => x.RouteArchiveId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RouteArchive");
+        }
+    }
+}
